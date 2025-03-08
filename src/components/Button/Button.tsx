19dc52914a25c220506346c5a7e595f0fc1c380a
anarkyst13 <anarkyst13@proton.me>
2025-03-08 12:28:10 +0200

    first commit

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyledButton } from './Button.styles';
+
+interface ButtonProps {
+  
+}
+
+const Button: React.FC<ButtonProps> = ({  }) => {
+  // PLACE FOR CUSTOM HOOKS
+  // Example: const [state, setState] = useState(initialValue);
+  // Example: const data = useCustomHook();
+
+  // PLACE FOR LOGIC
+  // Example: const handleClick = () => { ... };
+  // Example: useEffect(() => { ... }, []);
+
+  return (
+    <StyledButton>
+      
+    </StyledButton>
+  );
+};
+
+export default Button;
