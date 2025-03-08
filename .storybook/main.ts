24d458ceadac6cd9a6725127809dca6dd89673d3
anarkyst13 <anarkyst13@proton.me>
2025-03-08 12:30:32 +0200

    first commit

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,8 +3,6 @@ import type { StorybookConfig } from '@storybook/react-vite';
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
-    '@storybook/addon-actions',
-    '@storybook/addon-controls',
     '@storybook/addon-essentials',
     '@storybook/addon-onboarding',
     '@chromatic-com/storybook',
